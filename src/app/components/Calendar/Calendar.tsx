c91d7b99280f81414e2a440be8af5e39c64e2632
Po33ski <jaroslaw.popardowski@wp.pl>
2024-03-07 19:44:43 +0100

    isClient
    I added the condition isClient and a few small changes

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -3,6 +3,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import "../../weather_icons_data/css/weather-icons.css";
 import styles from "./Calendar.module.css";
 import { normalDateFormatted } from "@/app/functions/functions";
+import { useEffect, useState } from "react";
 
 export function Calendar({
   startDate,
@@ -15,47 +16,55 @@ export function Calendar({
   endDate: Date;
   setEndDate: (date: Date) => void;
 }) {
+  const [isClient, setIsClient] = useState<boolean>(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   const startDateV = normalDateFormatted(startDate);
   const endDateV = normalDateFormatted(endDate);
   const now = new Date();
 
   return (
     <div>
-      <div className={styles.desc}>
-        <h4>Start Date</h4>
-        <h4>End Date</h4>
-      </div>
-      <div className={styles.cell}>
+      {isClient && (
         <div>
-          <ReactDatePicker
-            dateFormat="dd/MM/yyyy"
-            selected={startDate}
-            value={startDateV}
-            className={styles.picker}
-            maxDate={now}
-            onChange={(date) => {
-              date = date ? date : new Date();
-              setStartDate(date);
-            }}
-          />
+          <div className={styles.desc}>
+            <h4>Start Date</h4>
+            <h4>End Date</h4>
+          </div>
+          <div className={styles.cell}>
+            <div>
+              <ReactDatePicker
+                dateFormat="dd/MM/yyyy"
+                selected={startDate}
+                value={startDateV}
+                className={styles.picker}
+                maxDate={now}
+                onChange={(date) => {
+                  date = date ? date : new Date();
+                  setStartDate(date);
+                }}
+              />
+            </div>
+            <div className={styles.arrow}>
+              <i className="wi wi-direction-right" />
+            </div>
+            <div>
+              <ReactDatePicker
+                dateFormat="dd/MM/yyyy"
+                selected={endDate}
+                value={endDateV}
+                className={styles.picker}
+                maxDate={now}
+                onChange={(date) => {
+                  date = date ? date : new Date();
+                  setEndDate(date);
+                }}
+              />
+            </div>
+          </div>
         </div>
-        <div className={styles.arrow}>
-          <i className="wi wi-direction-right" />
-        </div>
-        <div>
-          <ReactDatePicker
-            dateFormat="dd/MM/yyyy"
-            selected={endDate}
-            value={endDateV}
-            className={styles.picker}
-            maxDate={now}
-            onChange={(date) => {
-              date = date ? date : new Date();
-              setEndDate(date);
-            }}
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/app/components/SystemSelector/SystemSelector.tsx
+++ src/app/components/SystemSelector/SystemSelector.tsx
@@ -1,16 +1,26 @@
 import styles from "./SystemSelector.module.css";
+import { useContext, useEffect, useState } from "react";
 import { SYSTEMS } from "@/app/constants/unitSystems";
 import { UnitSystemContext } from "@/app/contexts/UnitSystemContext";
+<<<<<<< Updated upstream
 import { useLocalStorage } from "@/app/hooks/useLocalStorage";
+=======
+>>>>>>> Stashed changes
 import { UnitSystemContextType } from "@/app/types/types";
-import { useContext } from "react";
 
 export function SystemSelector() {
   const unitSystemContext = useContext<UnitSystemContextType | null>(
     UnitSystemContext
   );
+<<<<<<< Updated upstream
   //const [,setToLocalStorage] = useLocalStorage("data", "METRIC" );
 
+=======
+  const [isClient, setIsClient] = useState<boolean>(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+>>>>>>> Stashed changes
   const unitSystem =
     unitSystemContext?.unitSystem.data === "US" ||
     unitSystemContext?.unitSystem.data === "METRIC" ||
@@ -19,24 +29,33 @@ export function SystemSelector() {
       : "METRIC";
   return (
     <div>
-      <select
-        value={unitSystem}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-          unitSystemContext?.unitSystem.setToLocalStorage(
-            e.currentTarget.value
-          );
-        }}
-        className={styles.systemSelector}
-      >
-        <option value={SYSTEMS.METRIC}>{SYSTEMS.METRIC}</option>
-        <option value={SYSTEMS.UK}>{SYSTEMS.UK}</option>
-        <option value={SYSTEMS.US}>{SYSTEMS.US}</option>
-      </select>
+      {isClient && (
+        <select
+          value={unitSystem}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            unitSystemContext?.unitSystem.setToLocalStorage(
+              e.currentTarget.value
+            );
+          }}
+          className={styles.systemSelector}
+        >
+          <option value={SYSTEMS.METRIC}>{SYSTEMS.METRIC}</option>
+          <option value={SYSTEMS.UK}>{SYSTEMS.UK}</option>
+          <option value={SYSTEMS.US}>{SYSTEMS.US}</option>
+        </select>
+      )}
     </div>
   );
 }
 
 /*
+
+            value={
+              typeof unitSystemContext?.unitSystem.data === "string"
+                ? unitSystemContext?.unitSystem.data
+                : "METRIC"
+            }
+
         <option value={SYSTEMS.METRIC}>
           {SYSTEMS.METRIC}
           {" ("}
