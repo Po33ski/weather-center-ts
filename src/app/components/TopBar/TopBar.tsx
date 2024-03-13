4e902e1b5b65886b421c91723cd1d6734baa6ba0
Po33ski <jaroslaw.popardowski@wp.pl>
2024-03-13 17:57:37 +0100

    TopBar image
    I changed the background of the topbar using jpg image

--- src/app/components/TopBar/TopBar.tsx
+++ src/app/components/TopBar/TopBar.tsx
@@ -2,5 +2,12 @@ import { ReactNode } from "react";
 import styles from "./TopBar.module.css";
 
 export function TopBar({ children }: { children: ReactNode }) {
-  return <div className={styles.topBar}>{children}</div>;
+  return (
+    <div
+      className={styles.topBar}
+      style={{ backgroundImage: "url('/main_photo.jpg')" }}
+    >
+      {children}
+    </div>
+  );
 }
